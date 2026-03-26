beef0966e00f932d1f484642ccd41b7e4f1db140
Ruud van Asseldonk <dev@veniogames.com>
2026-03-26 22:44:29 +0100

    Give commits parents if possible
    I am wondering though whether we should just omit the entire commit part
    and let plain Git take care of that.

--- src/main.rs
+++ src/main.rs
@@ -101,13 +101,20 @@ fn commit_tree(repo: &Repository, tree_oid: git2::Oid) -> Result<git2::Oid> {
     let tree = repo.find_tree(tree_oid)?;
     let sig = repo.signature()?;
 
+    let parent = repo
+        .find_reference("refs/heads/main")
+        .ok()
+        .map(|r| r.peel_to_commit())
+        .transpose()?;
+    let parents: Vec<&git2::Commit> = parent.iter().collect();
+
     Ok(repo.commit(
         Some("refs/heads/main"),
         &sig,
         &sig,
         "Update cluster state",
         &tree,
-        &[],
+        &parents,
     )?)
 }
 
