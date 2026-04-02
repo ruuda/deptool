782bc8d1e9c39dd86e29d96b7831f485975c1810
Ruud van Asseldonk <dev@veniogames.com>
2026-04-02 20:54:43 +0200

    Add Nix flake for static build
    Adapted from Minimizer which also is a Rust binary that uses libgit2.
    Fortunately Deptool has simpler dependencies, and it seems upstream
    Nixpkgs also changed the libgit2 package so this http parser is no
    longer an issue either.
    
    It fails to compile though because the tests don't work without a Git
    user configured.

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Deptool";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-25.11";
+
+  # Flake adapted from Minimizer <https://codeberg.org/ruuda/minimizer>,
+  # licensed Apache 2.0.
+  outputs = { self, nixpkgs }: 
+    let
+      name = "deptool";
+      version = "0.1.0";
+      pkgs = (import nixpkgs { system = "x86_64-linux" ; }).pkgsStatic;
+    in
+      {
+        packages.x86_64-linux.default = pkgs.rustPlatform.buildRustPackage rec {
+          inherit name version;
+          src = pkgs.lib.sourceFilesBySuffices ./. [
+            ".rs"
+            "Cargo.lock"
+            "Cargo.toml"
+          ];
+          cargoLock = { lockFile = ./Cargo.lock; };
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = with pkgs; [
+            libgit2
+            libssh2
+            openssl
+            pcre
+            zlib
+          ];
+          RUSTFLAGS = "-lm -lssl -lc";
+        };
+      };
+}
